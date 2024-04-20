ff029afcf832e1b067e7694cbab9c39da560bbd0
ARGAO <jgabrielaragao@gmail.com>
2024-04-20 17:14:31 -0300

    oi

---
+++ ExemploPOO/Models/ContaCorrente.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numeroConta, decimal saldo)
+        {
+            NumeroConta = numeroConta;
+            this.saldo = saldo;
+        }
+        public int NumeroConta { get; set; }
+        private decimal saldo;
+
+
+        public void Sacar(decimal valor){
+            if(saldo >= valor){
+                saldo -= valor;
+                Console.WriteLine("Saque realizado com sucesso!");
+            }else{
+                Console.WriteLine("Saldo insuficiente!");
+            }
+        }
+
+        public void ExibirSaldo(){
+            Console.WriteLine($"Saldo: {saldo}");
+        }   
+    }
+}
