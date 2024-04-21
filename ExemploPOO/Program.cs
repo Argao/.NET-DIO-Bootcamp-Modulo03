35a6cb93e9a180919e75feed9b846c4c6979f6a0
ARGAO <jgabrielaragao@gmail.com>
2024-04-21 19:38:16 -0300

    herança e polimorfismo

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -1,7 +1,34 @@
-﻿using Models;
+﻿using ExemploPOO.Models;
+using Models;
 
-ContaCorrente c1 = new ContaCorrente(123,1000);
 
-c1.ExibirSaldo();
-c1.Sacar(500);
-c1.ExibirSaldo();
+
+
+Aluno a1 = new Aluno();
+a1.Nome = "João";
+a1.Idade = 20;
+a1.Nota = 8.5;
+a1.Email = "oi@gmail.com";
+a1.Apresentar();
+
+
+Professor p1 = new Professor();
+p1.Nome = "Maria";
+p1.Idade = 30;
+p1.Salario = 5000;
+p1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123,1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(500);
+// c1.ExibirSaldo();
--- ExemploPOO/obj/Debug/net8.0/ExemploPOO.AssemblyInfo.cs
+++ ExemploPOO/obj/Debug/net8.0/ExemploPOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploPOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25b971d98804e3900071bafe4f3c0ef8aec8ff1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff029afcf832e1b067e7694cbab9c39da560bbd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploPOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploPOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
